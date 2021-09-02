c4eb54455bfa5fc4fdae94e3a363b53a494cdd90
Damian Wasilewicz <damian_wasilewicz@brown.edu>
2021-09-02 14:58:57 -0400

    Updating staff pics and bios

--- staff.js
+++ staff.js
@@ -17,15 +17,12 @@ function restoreImage(login) {
 
 function createStaffCard(staffObject) {
   let {
-    name, login, pronouns, hometown, year, blurb, quote
+    name, login, pronouns, intro
   } = staffObject;
 
   // Formatting and seeting defaults
   pronouns = pronouns.toLowerCase();
-  if (hometown === '') { hometown = 'Earth'; }
-  if (year === '') { year = 'The future'; }
-  if (blurb === '') { blurb = `I'm a human on planet Earth!`; }
-  if (quote === '') { quote = 'The world may never know!'; }
+  if (intro === '') { intro = 'The mystery deepens...'; }
 
   const imagePath = `img/portrait/${login}.jpg`;
   return (`
@@ -43,10 +40,7 @@ function createStaffCard(staffObject) {
       <div class="staff-text">
         <p><strong>${name}</strong> (${login}) </p>
         <p><strong>Pronouns:</strong> ${pronouns} </p>
-        <p><strong>Year:</strong> ${year} </p>
-        <p><strong>Hometown:</strong> ${hometown} </p>
-        <p><strong>About:</strong> ${blurb} </p>
-        <p><strong>What will Blueno do in space?</strong> ${quote} </p>
+        <p><strong>About:</strong> ${intro} </p>
       </div>
     </div>
   `);
@@ -54,16 +48,17 @@ function createStaffCard(staffObject) {
 
 $(document).ready(function () {
   const instructors = ['dritchi1'];
-  const htas = ['kdu3', 'bblinn', 'glee43', 'kpal1'];
+  const htas = ['lliu58', 'ubhalla'];
   $.ajax({
     type: "GET",
-    url: "./staff_info.csv",
+    url: "./staff_info_F2021.csv",
     dataType: "text",
     success: function (response) {
       const table = $.csv.toArrays(response).splice(1);
       table.forEach(row => {
-        const [name, login, email, pronouns, hometown, year, blurb, quote] = row;
-        const staffObject = { name, login, email, pronouns, hometown, year, blurb, quote };
+        const [timestamp, fullname, name, pronouns, login, profpic, intro,
+          sgd, cnn, lstm, transformers, graphs, rl, gans, web] = row;
+        const staffObject = { name, login, pronouns, intro};
         if (instructors.includes(login)) {
           $('#instructors').append(createStaffCard(staffObject));
         } else if (htas.includes(login)) {
